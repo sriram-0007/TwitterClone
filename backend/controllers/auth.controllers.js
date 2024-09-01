03bf09e0598b6b07ce62a4662afda564a0367ac6
sriramyadav <sriramyadav885@gmail.com>
2024-09-01 22:08:41 +0530

    Complete Signup Form

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,7 +1,51 @@
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import { generateTokenAndSetCookie } from "../lib/generateToken.js";
 export const signup=async(req,res)=>{
-    res.json({
-        data:"You hit the signup endpoint"
-    });
+    try{
+        const {fullName,username,email,password}=req.body;
+        const emailRegex=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)){
+            return res.status(400).json({error:"Invalid email format"});
+        }
+        const existingUser=await User.findOne({username});
+        if(existingUser){
+            return res.staus(400).json({error:"Username is already taken"});
+        }
+        const existingEmail=await User.findOne({email});
+        if(existingEmail){
+            return res.staus(400).json({error:"Email already Exists"});
+        }
+
+        //hash password
+        const salt=await bcrypt.genSalt(10);
+        const hashedPassword=await bcrypt.hash(password,salt);
+
+
+        const newUser=new User({
+            fullName,
+            username,
+            email,
+            password:hashedPassword
+        })
+        if(newUser){
+            generateTokenAndSetCookie(newUser._id,res);
+            await newUser.save();
+            res.status(201).json({
+                _id:newUser._id,
+                fullName:newUser.fullName,
+                username:newUser.username,
+                followers:newUser.followers,
+                following:newUser.following,
+                profileImg:newUser.profileImg,
+                coverImg:newUser.coverImg
+            })
+        }else{
+            res.staus(400).json({error:"Invalid user data"});
+        }
+    }catch(error){
+        res.status(500).json({error:"Something went wrong in the server"});
+    }
 }
 
 export const login=async(req,res)=>{
