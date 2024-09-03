f10f17ed5d7ca5bdcc1b1d9e52cd3aa1f29adf12
sriramyadav <sriramyadav885@gmail.com>
2024-09-03 22:47:20 +0530

    Completed the authentication routes

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -16,6 +16,7 @@ export const signup=async(req,res)=>{
         if(existingEmail){
             return res.staus(400).json({error:"Email already Exists"});
         }
+       
 
         //hash password
         const salt=await bcrypt.genSalt(10);
@@ -49,13 +50,45 @@ export const signup=async(req,res)=>{
 }
 
 export const login=async(req,res)=>{
-    res.json({
-        data:"You hit the login endpoint"
-    });
+    try{
+        const {username,password}=req.body;
+        const user=await User.findOne({username});
+        const isPasswordCorrect=await bcrypt.compare(password,user?.password || "");
+        if(!user|| !isPasswordCorrect){
+            return res.status(400).json({error:"Invalid credentials"});
+        }
+        generateTokenAndSetCookie(user._id,res);
+        res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            username:user.username,
+            followers:user.followers,
+            following:user.following,
+            profileImg:user.profileImg,
+            coverImg:user.coverImg
+        });
+    }catch(error){
+        console.log("error in login controller",error.message);
+        res.status(500).json({error:"Internal Server Error"});
+    }
 }
 
 export const logout=async(req,res)=>{
-    res.json({
-        data:"You hit the logout endpoint"
-    });
+    try {
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({message:"Logged out successfully"});
+    } catch (error) {
+        console.log("Error in logout controller",error.message);
+        res.status(500).json({error:"Internal Server Error"});
+    }
+}
+
+export const getMe=async(req,res)=>{
+    try{
+        const user=await User.findById(req.user._id).select("-password");
+        res.status(200).json(user);
+    }catch(error){
+        console.log("Error in getMe controller",error.message);
+        res.status(500).json({error:"Internal Server Error"});
+    }
 }
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import {signup,login,logout} from "../controllers/auth.controllers.js"
+import {getMe,signup,login,logout} from "../controllers/auth.controllers.js"
+import { protectRoute } from "../middleware/protectRoute.js";
 const router=express.Router();
 
+router.get("/me",protectRoute,getMe);
 router.post("/signup",signup)
-
 router.post("/login",login)
 router.post("/logout",logout)
+
 
 export default router;
